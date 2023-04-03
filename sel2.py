a93b125bb964ad9a59d6470820cb511e3f696cc0
Randheer kumar <randheer.dakshana15@gmail.com>
2023-04-03 15:41:02 +0530

    some modification

--- sel2.py
+++ sel2.py
@@ -1,18 +1,18 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome(executable_path = '/Users/randheerkumar/Downloads/chromedriver')
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+service = Service(executable_path='/Users/randheerkumar/Downloads/chromedriver')
+driver = webdriver.Chrome(service=service)
 
 driver.get('https://www.amazon.in/')
 driver.maximize_window()
 
 time.sleep(5)
 
-select=driver.find_element_by_link_text('Electronics')
-
+select = driver.find_element(By.LINK_TEXT, 'Electronics')
 select.click()
-time.sleep(5)
 
-select1 = driver.find_element_by_link_text('Audio')
-select1.click()
 time.sleep(5)
